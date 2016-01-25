96d8394ba45e8da34476e32478c78a5db0c36552
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-01-25 14:13:37 +0900

    :construction: compare version of yaml and app

--- lib/ruboty/gominohi/yaml.rb
+++ lib/ruboty/gominohi/yaml.rb
@@ -22,9 +22,17 @@ module Ruboty
         end
 
         raise "#{YAML_FILE} is not found" if @data.nil?
+
+        yaml_version     = parse_and_truncate_patch_version(@data[:version])
+        gominohi_version = parse_and_truncate_patch_version(::Ruboty::Gominohi::VERSION)
+        raise 'version of `.gominohi.yml` is invalid' unless yaml_version == gominohi_version
       end
 
       private
+
+      def parse_and_truncate_patch_version(version_string)
+        version_ints = version_string.split('.').map(&:to_i).slice(0..1)
+      end
 
       def symbolize_keys(hash)
         hash.each_with_object({}) { |(k, v), result| result[k.to_sym] = v }
